d719bf43d506f9f4e81951ffae970af81ce96b0f
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-07 22:24:18 +0600

    Connect database with server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 7007;
@@ -8,6 +9,23 @@ const port = process.env.PORT || 7007;
 // -------Middle-Ware-----
 app.use(cors());
 app.use(express.json());
+
+
+// -------Database Connection-------
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.vvll70g.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function dbConnect() {
+    try {
+        await client.connect();
+        console.log('Database Connected');
+
+    } catch (error) {
+        console.log(error.name, error.message);
+    }
+}
+dbConnect();
+
 
 // ------------End-Points-------------
 
